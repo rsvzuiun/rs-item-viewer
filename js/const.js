96d017d2dfb50391a93454ce1ae003ed0b365ad9
rsvzuiun <87635034+rsvzuiun@users.noreply.github.com>
2022-10-19 18:36:41 +0900

    v0.0782

--- js/const.js
+++ js/const.js
@@ -1,6 +1,6 @@
 // @ts-check
 /* eslint-disable no-unused-vars */
-const version = "2022/09/07 v0.0780";
+const version = "2022/10/19 v0.0782";
 
 let itemdata_url = "data/itemData.json";
 const textdata_url = "data/textData.json";
