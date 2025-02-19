af5804f1df410f3719e9754ae9c268be0d92aefc
rsvzuiun <87635034+rsvzuiun@users.noreply.github.com>
2025-02-19 20:29:05 +0900

    fuck old browsers

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    target: ["es2022", "edge89", "firefox89", "chrome89", "safari15"],
+  },
   base: "/rs-item-viewer/",
 });
