6031266c2b658118f6a3eac8f547a40760c36261
rsvzuiun <87635034+rsvzuiun@users.noreply.github.com>
2026-02-19 19:27:42 +0900

    v0.0870 21th

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const version = "2025/12/11 v0.0868";
+export const version = "2026/02/19 v0.0870";
 
 export const itemdata_url = "data/itemData.json";
 export const itemdatakr_url = "data/itemData-kr.json";
